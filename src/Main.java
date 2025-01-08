a60aecd47cec1581f805ddb974b94a6c6c8ee643
Dmytro Kulish <mitiakylish2@gmail.com>
2025-01-08 15:14:30 +0200

    Added task2 solution

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        getLengthOfStrings(new String[]{"this", "and", "that", "and"});
+       // getLengthOfStrings(new String[]{"this", "and", "that", "and"});
+        createMap(new String[]{"man", "moon", "good", "night"});
 
     }
     //Task 1
-    public static void getLengthOfStrings(String[] getLengthOfStrings) {
+    public static void getLengthOfStrings(String[] array) {
         Map<String, Integer> map = new HashMap<>();
-        for (String key : getLengthOfStrings) {
+        for (String key : array) {
             map.put(key, key.length());
+        }
+        System.out.println(map);
+    }
+
+    //Task 2
+    public static void createMap(String[] array) {
+        Map<Character, Character> map = new HashMap<>();
+        for (String key : array) {
+            char ch = key.charAt(0);
+            char ch2 = key.charAt(key.length()-1);
+            map.put(ch, ch2);
         }
         System.out.println(map);
     }
