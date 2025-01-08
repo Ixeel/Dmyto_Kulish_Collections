d92c82543f39b358b0b189910c69a8798863490b
Dmytro Kulish <mitiakylish2@gmail.com>
2025-01-08 14:32:16 +0200

    Added task1 solution

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,16 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3,14,15,92,6));
-        for (int i: numbers)
-            System.out.println(i);
+        getLengthOfStrings(new String[]{"this", "and", "that", "and"});
 
-        ArrayList<String> string = new ArrayList<>(Arrays.asList("Дмитро", "Куліш", "Володимирович"));
-        for (String i: string)
-            System.out.println(i);
-
-        HashSet<Integer> numbersHashSet = new HashSet<Integer>();
-        numbersHashSet.add(1);
-        numbersHashSet.add(2);
-        numbersHashSet.add(3);
-        // дублікат
-        numbersHashSet.add(1);
-        System.out.println(numbersHashSet);
-
-
-        HashMap<String, String> ukrainianCitiesAndNumbers = new HashMap<>();
-        ukrainianCitiesAndNumbers.put("Харків","057");
-        ukrainianCitiesAndNumbers.put("Вінниця", "0432");
-        ukrainianCitiesAndNumbers.put("Київ", "044");
-        System.out.println(ukrainianCitiesAndNumbers);
     }
-
+    //Task 1
+    public static void getLengthOfStrings(String[] getLengthOfStrings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String key : getLengthOfStrings) {
+            map.put(key, key.length());
+        }
+        System.out.println(map);
+    }
 }
